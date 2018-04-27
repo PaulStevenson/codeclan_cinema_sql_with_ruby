2f76f66eda15906f1d9a2aa9844645460f4f2b34
Paul <rpmstevenson@gmail.com>
2018-04-27 10:59:51 +0100

    cinema sql set-up

---
+++ db/cinema.sql
@@ -0,0 +1,21 @@
+DROP TABLE tickets;
+DROP TABLE customers;
+DROP TABLE films;
+
+CREATE TABLE films(
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT2
+)
+
+CREATE TABLE customers(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  fund VARCHAR(255)
+)
+
+CREATE TABLE tickets(
+  id SERIAL4 PRIMARY KEY,
+  customer_id REFERENCES customers(id) ON DELETE CASCADE,
+  film_id REFERENCES films(id) ON DELETE CASCADE
+)
